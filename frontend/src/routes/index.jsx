8bf4eef75a8b87f407bc614858cabbb375949f41
SerraZ3 <serra.henrique3@gmail.com>
2021-08-19 18:25:57 -0300

    Adicionando react-router-dom e configurado rotas

---
+++ frontend/src/routes/index.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Home from "../pages/Home";
+
+// import { Container } from './styles';
+
+function Routes() {
+  return (
+    <Router>
+      <Route path={["/", "/home"]} exact strict component={Home} />
+      <Route path={"/create-team"} exact strict component={Home} />
+      <Route path={"/create-table"} exact strict component={Home} />
+      <Route path={"/insert-team"} exact strict component={Home} />
+    </Router>
+  );
+}
+
+export default Routes;
